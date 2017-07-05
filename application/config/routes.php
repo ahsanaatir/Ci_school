87b2c5fdcb06a96906350ea2c8bb738dabcbef88
ahsanaatir <ahsanaatir@gmail.com>
2017-07-05 13:28:14 +0500

    Revert "Working On Add Attendance"
    This reverts commit d462f0b8d602f31f253f8555ad0ed17a5cd89c9c.

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,9 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['students/load_attendance'] = 'students/load_attendance';
+
+$route['teachers'] = "teachers/index";
 $route['students/search'] = 'students/search';
-$route['students/add'] = 'students/add';
+$route['students/add'] = 'students/add_student';
 $route['students'] = 'students/index';
 $route['home'] = 'home/index';
 $route['default_controller'] = 'users/login';
--- application/models/Student_model.php
+++ application/models/Student_model.php
@@ -10,36 +10,37 @@ class Student_model extends CI_Model
 
     public function get_all()
     {
-        $this->db->order_by('id', 'DESC');
-        $query = $this->db->get('students');
-        return $query->result_array();
-    }
-        public function get_all_students()
-    {
-        
-        $query = $this->db->get('student_personal_info');
-        return $query->result();
-    }
-    /*by asfand yar*/
-    public function load_attendance()
-    {
-       
-        $section_id = $this->input->post('section_id');
-        $session_id=$this->input->post('session_id');
-        $class_id = $this->input->post('class_id'); 
-         
-        $query = $this->db->select('student_personal_info.id , student_admission_info.student_id');
-        $query = $this->db->from('student_personal_info','student_admission_info');
-        $query = $this->db->join('student_personal_info','student_personal_info.id = student_admission_info.student_id');
-        
-        $query = $this->db->get_where('student_admission_info.session_id='.$session_id,'student_admission_info.class_id='.$class_id,'student_admission_info.section_id='.$section_id);
-        return $query->result();
-    }
-    /* end by asfand yar*/
 
+        $this->db->select('*');
+        $this->db->from('student_personal_info');
+        $this->db->join('student_admission_info', 'student_admission_info.student_id = student_personal_info.id');
+        $this->db->join('student_family_info', 'student_family_info.student_id = student_personal_info.id');
+        return $this->db->get()->result();
+    }
+
+//    public function save_student_personal_info()
+//    {
+//
+//    }
+//
+//    public function save_student_family_info()
+//    {
+//
+//    }
+//
+//    public function save_student_admission_info()
+//    {
+//
+//    }
+//
+//    public function save_student_fee_info()
+//    {
+//
+//    }
 
     public function save()
     {
+        //var_dump($_POST);
         $personal_info_data = array(
             'full_name' => $this->input->post('student_name'),
             'bay_form' => $this->input->post('b_form'),
@@ -50,9 +51,10 @@ class Student_model extends CI_Model
             'prev_class_name' => $this->input->post(''),
             'user_id' => 1
         );
-        $this->db->insert('students', $personal_info_data);
+        $this->db->insert('student_personal_info', $personal_info_data);
+        $std_id= $this->db->insert_id();
         $family_info_data = array(
-            'student_id' => 1,
+            'student_id' => $std_id,
             'father_name' => $this->input->post('father_name'),
             'father_cnic' => $this->input->post('father_cnic'),
             'father_profession' => $this->input->post('father_profession'),
@@ -63,9 +65,9 @@ class Student_model extends CI_Model
             'religion' => $this->input->post('religion'),
             'cast' => $this->input->post('cast')
         );
-        $this->db->insert('students', $family_info_data);
+        $this->db->insert('student_family_info', $family_info_data);
         $admission_info_data = array(
-            'student_id' => 1,
+            'student_id' => $std_id,
             'school_id' => 1,
             'session_id' => $this->input->post('session_id'),
             'class_id' => $this->input->post('class_id'),
@@ -74,14 +76,14 @@ class Student_model extends CI_Model
             'registration_no' => $this->input->post('registration_no'),
             'date_of_admission' => $this->input->post('date_of_admission')
         );
-        $this->db->insert('students', $admission_info_data);
+        $this->db->insert('student_admission_info', $admission_info_data);
         $fee_info_data = array(
-            'student_id' => 1,
+            'student_id' => $std_id,
             'admission_fee' => $this->input->post('admission_fee'),
             'tuition_fee' => $this->input->post('tuition_fee'),
             'prev_dues' => $this->input->post('prev_dues')
         );
-        $this->db->insert('students', $fee_info_data);
+        $this->db->insert('student_fee_info', $fee_info_data);
 
 
         return 1;
